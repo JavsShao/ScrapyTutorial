45f99e1f9225c01f07e90554b262867043b6abcf
hamdi <javs_shao@163.com>
2019-01-04 11:26:33 +0800

    items编写_字段

--- tutorial/tutorial/items.py
+++ tutorial/tutorial/items.py
@@ -8,7 +8,8 @@
 import scrapy
 
 
-class TutorialItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class QuteItem(scrapy.Item):
+
+    text = scrapy.Field()
+    author = scrapy.Field()
+    tags = scrapy.Field()

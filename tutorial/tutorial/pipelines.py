40b578e049ab94434a6dbfa18e574b532ea65f88
hamdi <javs_shao@163.com>
2019-01-04 13:24:10 +0800

    pipelines_MongoPipeline_初始化方法

--- tutorial/tutorial/pipelines.py
+++ tutorial/tutorial/pipelines.py
@@ -20,3 +20,7 @@ class TextPipeline(object):
         else:
             return DropItem('Missing Text')
 
+class MongoPipeline(object):
+    def __init__(self, mongo_uri, mongo_db):
+        self.mongo_uri = mongo_uri
+        self.mongo_db = mongo_db

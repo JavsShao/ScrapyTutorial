26f3769c6bd7436482548a1c23706555563d2358
hamdi <javs_shao@163.com>
2019-01-04 13:30:51 +0800

    配置文件_MongoDB_配置

--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -90,3 +90,6 @@ ITEM_PIPELINES = {
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+MONGO_URI = 'localhost'
+MONGO_DB = 'tutorial'

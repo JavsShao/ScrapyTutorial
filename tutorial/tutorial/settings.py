8e1969a5fa83fcc4975f26b063cc425a5a72677e
hamdi <javs_shao@163.com>
2019-01-04 13:30:15 +0800

    配置文件_ITEM_PIPELINES

--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -64,9 +64,11 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'tutorial.pipelines.TutorialPipeline': 300,
-#}
+ITEM_PIPELINES = {
+   # 'tutorial.pipelines.TutorialPipeline': 300,
+    'tutorial.pipelines.TextPipeline': 300,
+    'tutorial.pipelines.MongoPipeline': 400,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://doc.scrapy.org/en/latest/topics/autothrottle.html

647fe0d80cee6f79e25e51aa7c9db6d687c72f25
hamdi <javs_shao@163.com>
2019-01-04 12:57:03 +0800

    pipelines_初始化

--- tutorial/tutorial/pipelines.py
+++ tutorial/tutorial/pipelines.py
@@ -4,8 +4,10 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import pymongo
+from scrapy.exceptions import DropItem
 
 
-class TutorialPipeline(object):
-    def process_item(self, item, spider):
-        return item
+class TextPipeline(object):
+    def __init__(self):
+        self.limit = 50
